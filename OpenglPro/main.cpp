45be839175bdf00f2be24fd305da1f7c954712aa
zhao <zhao@self.com>
2018-02-05 21:03:31 +0800

    triangle training

--- OpenglPro/main.cpp
+++ OpenglPro/main.cpp
@@ -7,8 +7,13 @@
 float vertices[]={
 	0.5f,0.5f,0.0f,
 	0.5f,-0.5f,0.0f,
-	-0.5f,-0.5f,0.0f,
-	-0.5f,0.5f,0.0f
+	0.0f,0.0f,0.0f
+};
+
+float vertices1[]={
+	-0.5f,0.5f,0.0f,
+	0.0f,0.0f,0.0f,
+	-0.5f,-0.5f,0.0f
 };
 
 unsigned int indices[]={
@@ -28,6 +33,13 @@ char* fragment_shader = "#version 330 core \n\
 					  {\
 					      FragColor=vec4(1.0f,0.5,0.2f,1.0f); \
 					  }";
+
+char* fragment_shader2 = "#version 330 core \n\
+						out vec4 FragColor;\n \
+						void main() \n\
+						{\
+						FragColor=vec4(1.0f,1.0,0.0f,1.0f); \
+						}";
 
 unsigned int Drawstyle = GL_TRIANGLES;
 unsigned int keystate = 0;
@@ -100,25 +112,37 @@ int main(int _Argc,char** _argv)
 	glViewport(0,0,800,600);
 	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 
-	unsigned int Vbo;
-	glGenBuffers(1,&Vbo);
 
-	unsigned int ebo;
-	glGenBuffers(1,&ebo);
+
+	//unsigned int ebo;
+	//glGenBuffers(1,&ebo);
 
 	unsigned int VAO;
 	glGenVertexArrays(1,&VAO);
-
 	glBindVertexArray(VAO);
 
+	unsigned int Vbo;
+	glGenBuffers(1,&Vbo);
 	glBindBuffer(GL_ARRAY_BUFFER,Vbo);
 
 	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+	unsigned int VAO1;
+	glGenVertexArrays(1,&VAO1);
+	glBindVertexArray(VAO1);
+
+	unsigned int Vbo1;
+	glGenBuffers(1,&Vbo1);
+	glBindBuffer(GL_ARRAY_BUFFER,Vbo1);
+
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices1),vertices1,GL_STATIC_DRAW);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+	glEnableVertexAttribArray(0);
+
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -142,6 +166,17 @@ int main(int _Argc,char** _argv)
 		return -1;
 	}
 
+	unsigned int fragmentshader2;
+	fragmentshader2 = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentshader2,1,&fragment_shader2,NULL);
+	glCompileShader(fragmentshader2);
+
+	if (shaderError(fragmentshader2,GL_COMPILE_STATUS) == false)
+	{
+		return -1;
+	}
+
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 
@@ -154,6 +189,15 @@ int main(int _Argc,char** _argv)
 		return -1;
 	}
 
+	unsigned int shaderProgram2;
+	shaderProgram2 = glCreateProgram();
+
+	glAttachShader(shaderProgram2,vertexShader);
+	glAttachShader(shaderProgram2,fragmentshader2);
+	glLinkProgram(shaderProgram2);
+
+	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+
 	while(!glfwWindowShouldClose(window)){
 		processInput(window);
 
@@ -162,9 +206,13 @@ int main(int _Argc,char** _argv)
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-		glDrawElements(Drawstyle,6,GL_UNSIGNED_INT,0);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+		//glDrawElements(Drawstyle,6,GL_UNSIGNED_INT,0);
+		glDrawArrays(GL_TRIANGLES,0,3);
 
+		glUseProgram(shaderProgram2);
+		glBindVertexArray(VAO1);
+		glDrawArrays(GL_TRIANGLES,0,3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
